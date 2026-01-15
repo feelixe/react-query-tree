e6256d6e7e66d3dedacb014d32658249ab4f1172
Felix Eklöf <felixeklof@icloud.com>
2026-01-15 15:39:52 +0000

    ci: run publish in workflow

--- tag-and-release.ts
+++ tag-and-release.ts
@@ -2,9 +2,6 @@ import { $ } from "bun";
 import semver from "semver";
 import packageJson from "./packages/react-query-tree/package.json";
 
-const TAG_MAP = [[/^rc[0-9]+$/, "rc"] as const];
-const DEFAULT_TAG = "latest";
-
 async function doesTagAlreadyExist(version: string) {
 	try {
 		await $`gh api -X GET /repos/{owner}/{repo}/git/ref/tags/v${version}`.quiet();
@@ -12,26 +9,6 @@ async function doesTagAlreadyExist(version: string) {
 	} catch {
 		return false;
 	}
-}
-
-async function lint() {
-	console.info("Linting...");
-	await $`bun run lint`;
-	await $`bun run format`;
-}
-
-async function runTests() {
-	console.info("Running tests...");
-	await $`bun test`;
-}
-
-async function build() {
-	console.info("Building...");
-	await $`bun run build`;
-}
-
-async function publishToNpm(npmTag: string) {
-	await $`cd packages/react-query-tree && npm publish --tag ${npmTag}`;
 }
 
 async function tagAndPush(version: string) {
@@ -48,18 +25,10 @@ function getVersionAndTag(version: string) {
 	if (!parsedVersion) {
 		throw new Error(`Invalid version: ${version}`);
 	}
-	const tagString = version.split("-").at(1);
-	if (tagString === undefined) {
-		return { version: parsedVersion, tag: DEFAULT_TAG };
-	}
-	const matching = TAG_MAP.find(([regex]) => regex.test(tagString));
-	if (matching === undefined) {
-		throw new Error(`Invalid tag: ${tagString}`);
-	}
-	return { version: parsedVersion, tag: matching[1] };
+	return { version: parsedVersion };
 }
 
-const { version: versionString, tag } = await getVersionAndTag(packageJson.version);
+const { version: versionString } = await getVersionAndTag(packageJson.version);
 
 const versionIsBusy = await doesTagAlreadyExist(versionString);
 if (versionIsBusy) {
@@ -67,11 +36,7 @@ if (versionIsBusy) {
 	process.exit(1);
 }
 
-const isPreRelease = tag !== DEFAULT_TAG;
+const isPreRelease = packageJson.publishConfig.tag !== "latest";
 
-await lint();
-await runTests();
-await build();
 await tagAndPush(versionString);
-await publishToNpm(tag);
 await createRelease(versionString, isPreRelease);
